97fb92192c2e70ddcb477628070920fd30778974
zokam1411 <zokam1411@gmail.com>
2021-05-06 11:59:49 +0000

    Update context.py to show products in basket

--- bag/contexts.py
+++ bag/contexts.py
@@ -1,5 +1,7 @@
 from decimal import Decimal
 from django.conf import settings
+from django.shortcuts import get_object_or_404
+from products.models import Product
 
 
 def bag_contents(request):
@@ -8,6 +10,18 @@ def bag_contents(request):
     total = 0
     product_count = 0
     delivery = settings.STANDARD_DELIVERY
+    bag = request.session.get('bag', {})
+
+    for item_id, quantity in bag.items():
+        product = get_object_or_404(Product, pk=item_id)
+        total += quantity * product.price
+        product_count += quantity
+        bag_items.append({
+            'item_id': item_id,
+            'quantity': quantity,
+            'product': product,
+        })
+
     grand_total = delivery + total
 
     context = {
