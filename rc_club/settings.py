a5d704c5a615bc2768f630409041d3ab5e2d43c8
zokam1411 <zokam1411@gmail.com>
2021-05-16 00:23:31 +0000

    Add profile app

--- rc_club/settings.py
+++ rc_club/settings.py
@@ -49,6 +49,7 @@ INSTALLED_APPS = [
     'bag',
     'shop',
     'checkout',
+    'profiles',
 
     # Others
     'crispy_forms',
--- rc_club/urls.py
+++ rc_club/urls.py
@@ -26,4 +26,5 @@ urlpatterns = [
     path('bag/', include('bag.urls')),
     path('shop/', include('shop.urls')),
     path('checkout/', include('checkout.urls')),
+    path('profile/', include('profiles.urls')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
