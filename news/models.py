f3fe1b664bb4ff2f207a8309994673d91954b24d
zokam1411 <zokam1411@gmail.com>
2021-05-18 23:54:39 +0000

    Add news models and register in admin

--- news/models.py
+++ news/models.py
@@ -1,3 +1,27 @@
 from django.db import models
+from django.contrib.auth.models import User
+
 
 # Create your models here.
+
+class News(models.Model):
+    author = models.ForeignKey(User, on_delete=models.SET_NULL,
+                               null=True,
+                               blank=True,
+                               )
+    date_created = models.DateTimeField(auto_now_add=True)
+    title = models.CharField(max_length=250,
+                             unique=True,
+                             null=False,
+                             blank=False,
+                             )
+    sub_title = models.CharField(max_length=250,
+                                 unique=True,
+                                 null=False,
+                                 blank=False,
+                                 )
+    content = models.TextField(null=False, blank=False,)
+    last_updated = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.title
