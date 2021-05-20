385ff76455246e8f0b6bd5286d8e7ce15f5b70eb
zokam1411 <zokam1411@gmail.com>
2021-05-20 19:41:34 +0000

    Style form with border and placeholders

--- news/forms.py
+++ news/forms.py
@@ -8,3 +8,18 @@ class NewsForm(forms.ModelForm):
         model = News
         fields = '__all__'
         exclude = ('author',)
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
+        self.fields['introduction'].widget.attrs['placeholder'] = (
+            'This will be visible as a first paragraph'
+        )
+        self.fields['introduction'].widget.attrs['rows'] = 4
+
+        self.fields['content'].widget.attrs['placeholder'] = (
+            'You can use HTML tags to format text'
+        )
+
+        for field_name, field in self.fields.items():
+            field.widget.attrs['class'] = 'rounded-0'
