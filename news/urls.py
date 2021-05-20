e822197442230d363af81f587bc311433d016f37
zokam1411 <zokam1411@gmail.com>
2021-05-20 18:59:01 +0000

    Add add post functionality

--- news/urls.py
+++ news/urls.py
@@ -4,4 +4,5 @@ from . import views
 urlpatterns = [
     path('', views.news, name='news'),
     path('<int:post_id>/', views.post_detail, name='post_detail'),
+    path('add_post/', views.add_post, name='add_post'),
 ]
--- news/views.py
+++ news/views.py
@@ -1,8 +1,11 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, redirect, reverse, get_object_or_404
+from django.contrib import messages
 from .models import News
 
+from .forms import NewsForm
 
 # Create your views here.
+
 
 def news(request):
     """ A view to show all news """
@@ -16,7 +19,7 @@ def news(request):
     return render(request, 'news/news.html', context)
 
 
-def news_detail(request, post_id):
+def post_detail(request, post_id):
     """ A view to show individual news """
 
     post = get_object_or_404(News, pk=post_id)
@@ -25,4 +28,31 @@ def news_detail(request, post_id):
         'post': post,
     }
 
-    return render(request, 'news/news_detail.html', context)
+    return render(request, 'news/post_detail.html', context)
+
+
+def add_post(request):
+    """ Add a post to news """
+
+    if not request.user.is_superuser:
+        messages.error(request, 'Sorry, only club committee member can do that.')
+        return redirect(reverse('home'))
+    if request.method == 'POST':
+        form = NewsForm(request.POST, request.FILES)
+        if form.is_valid():
+            post = form.save()
+            post.author = request.user
+            post.save()
+            messages.success(request, 'Post successfully added!')
+            return redirect(reverse('post_detail', args=[post.id]))
+        else:
+            messages.error(request, 'Failed to add post. Please ensure the form is valid.')
+    else:
+        form = NewsForm()
+        
+    template = 'news/add_post.html'
+    context = {
+        'form': form,
+    }
+
+    return render(request, template, context)
