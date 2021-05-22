83eae5ca70c4e53529c8cdc1c8fdc541639e1f06
zokam1411 <zokam1411@gmail.com>
2021-05-22 13:53:27 +0000

    Use fetch API to get public key

--- membership/urls.py
+++ membership/urls.py
@@ -2,5 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.membership, name='membership')
+    path('', views.membership, name='membership'),
+    path('config/', views.stripe_config),
+
 ]
--- membership/views.py
+++ membership/views.py
@@ -1,9 +1,22 @@
 from django.shortcuts import render
+from django.conf import settings
+from django.contrib.auth.decorators import login_required
+from django.http.response import JsonResponse
+from django.views.decorators.csrf import csrf_exempt
 
 # Create your views here.
+
 
 def membership(request):
     """ A view to return membership page """
 
     return render(request, 'membership/membership.html')
+
+
+@csrf_exempt
+def stripe_config(request):
+    if request.method == 'GET':
+        stripe_config = {'publicKey': settings.STRIPE_PUBLIC_KEY}
+        return JsonResponse(stripe_config, safe=False)
+
     
