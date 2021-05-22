02bc9b07b2bf71b55f6628239630df53e0f9e737
zokam1411 <zokam1411@gmail.com>
2021-05-22 14:17:18 +0000

    Add event handler on button click

--- membership/static/membership/js/stripe_membership.js
+++ membership/static/membership/js/stripe_membership.js
@@ -1,9 +1,31 @@
 console.log("Hello!");
 
 // Get Stripe publishable key
-fetch("/config/")
-.then((result) => { return result.json(); })
-.then((data) => {
-  // Initialize Stripe.js
-  const stripe = Stripe(data.publicKey);
-});
+fetch("/membership/config/")
+    .then((result) => {
+        return result.json();
+    })
+    .then((data) => {
+        // Initialize Stripe.js
+        const stripe = Stripe(data.publicKey);
+        let submitBtn = document.querySelector("#submitBtn");
+        if (submitBtn !== null) {
+            submitBtn.addEventListener("click", () => {
+                // Get Checkout Session ID
+                fetch("/membership/create-checkout-session/")
+                    .then((result) => {
+                        return result.json();
+                    })
+                    .then((data) => {
+                        console.log(data);
+                        // Redirect to Stripe Checkout
+                        return stripe.redirectToCheckout({
+                            sessionId: data.sessionId
+                        })
+                    })
+                    .then((res) => {
+                        console.log(res);
+                    });
+            });
+        }
+    });
