9e86f8c03e8859235faa82b6afead2a0214a83d8
zokam1411 <zokam1411@gmail.com>
2021-05-22 13:12:20 +0000

    Create model StripeCustomer

--- membership/admin.py
+++ membership/admin.py
@@ -1,3 +1,5 @@
 from django.contrib import admin
+from membership.models import StripeCustomer
 
-# Register your models here.
+
+admin.site.register(StripeCustomer)
--- membership/models.py
+++ membership/models.py
@@ -1,3 +1,12 @@
+from django.contrib.auth.models import User
 from django.db import models
 
-# Create your models here.
+
+class StripeCustomer(models.Model):
+    user = models.OneToOneField(to=User, on_delete=models.CASCADE)
+    stripeCustomerId = models.CharField(max_length=255)
+    stripeSubscriptionId = models.CharField(max_length=255)
+
+    def __str__(self):
+        return self.user.username
+        
