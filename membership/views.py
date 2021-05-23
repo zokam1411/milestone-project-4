868f5697f38fceda7a4797ef7b5922ea110182ff
zokam1411 <zokam1411@gmail.com>
2021-05-23 16:59:32 +0000

    Save membership to user profile

--- membership/views.py
+++ membership/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.conf import settings
 from django.contrib.auth.decorators import login_required
 from django.http.response import JsonResponse
@@ -7,6 +7,7 @@ from django.views.decorators.csrf import csrf_exempt
 from django.http import HttpResponse
 from django.contrib.auth.models import User
 from membership.models import StripeCustomer
+from profiles.models import UserProfile
 
 import stripe
 
@@ -50,7 +51,8 @@ def create_checkout_session(request):
         try:
             checkout_session = stripe.checkout.Session.create(
                 client_reference_id=request.user.id if request.user.is_authenticated else None,
-                success_url=domain_url + 'success?session_id={CHECKOUT_SESSION_ID}',
+                success_url=domain_url +
+                'success?session_id={CHECKOUT_SESSION_ID}',
                 cancel_url=domain_url + 'cancel/',
                 payment_method_types=['card'],
                 mode='subscription',
@@ -111,5 +113,12 @@ def stripe_webhook(request):
             stripeCustomerId=stripe_customer_id,
             stripeSubscriptionId=stripe_subscription_id,
         )
+
+        profile = get_object_or_404(UserProfile, user=user)
+        profile.membership = 'active'
+        profile.save()
+
+    if event['type'] == 'customer_subscription_deleted':
+        session = event['data']['object']
 
     return HttpResponse(status=200)
