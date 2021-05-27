e6099908f2a7f8da65b5bd3a8ed1ca7d2566f5ff
zokam1411 <zokam1411@gmail.com>
2021-05-27 10:58:10 +0000

    Add discount to Order

--- checkout/models.py
+++ checkout/models.py
@@ -25,6 +25,7 @@ class Order(models.Model):
     county = models.CharField(max_length=80, null=True, blank=True)
     date = models.DateTimeField(auto_now_add=True)
     delivery_cost = models.DecimalField(max_digits=6, decimal_places=2, null=False, default=0)
+    discount = models.DecimalField(max_digits=6, decimal_places=2, null=False, default=0)
     order_total = models.DecimalField(max_digits=10, decimal_places=2, null=False, default=0)
     grand_total = models.DecimalField(max_digits=10, decimal_places=2, null=False, default=0)
     original_bag = models.TextField(null=False, blank=False, default='')
