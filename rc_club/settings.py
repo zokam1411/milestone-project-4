2c49e07d5ab0cdcda73b72e355d890d61c6a2944
zokam1411 <zokam1411@gmail.com>
2021-05-27 11:45:14 +0000

    Remove secret key and set debug

--- rc_club/settings.py
+++ rc_club/settings.py
@@ -24,10 +24,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-d&9#g#2ree%!*u4kpj)s&)xe$4s2iacrhl^_l2s*ju&x0&y=c$'
+SECRET_KEY = os.environ.get('SECRET_KEY', '')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = 'DEVELOPMENT' in os.environ
 
 ALLOWED_HOSTS = ['ffrccc-project.herokuapp.com', 'localhost']
 
