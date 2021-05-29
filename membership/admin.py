c557c981c359751e7a468253a27089b1b4b76774
zokam1411 <zokam1411@gmail.com>
2021-05-29 15:27:57 +0000

    Add list display

--- membership/admin.py
+++ membership/admin.py
@@ -1,5 +1,11 @@
 from django.contrib import admin
 from membership.models import StripeCustomer
 
+class StripeCustomerAdmin(admin.ModelAdmin):
+    list_display = (
+        'user',
+        'stripeCustomerId',
+        'stripeSubscriptionId',
+    )
 
-admin.site.register(StripeCustomer)
+admin.site.register(StripeCustomer, StripeCustomerAdmin)
